bc2b70470ca9aa945327bc1269ee5a75955a6cc4
zmoon <zmoon92@gmail.com>
2021-03-31 14:37:41 -0400

    Add some info to blpd docstring

--- blpd/__init__.py
+++ blpd/__init__.py
@@ -1,5 +1,19 @@
 """
 Lagrangian (stochastic) particle dispersion model to model bees encountering floral scents
+
+The model class `blpd.main.Model` is included in this namespace for convenience.
+>>> import blpd
+>>> m = blpd.Model()
+>>> m.run().plot()
+
+Additional plots can be created using `blpd.plot`.
+Some of these require a chem dataset.
+>>> ds = m.to_xr()  # `xr.Dataset` of LPD results
+>>> dsc = blpd.chem.calc_relative_levels_fixed_oxidants(ds)  # compute chem dataset
+>>> blpd.plot.ws_hist_all(ds)  # plot wind speed histograms
+>>> blpd.plot.conc_2d(dsc)  # plot 2-d chemical species levels
+
+See [the examples](https://github.com/zmoon/blpd/tree/master/examples) for more info.
 """
 # Define the namespace for the module `blpd`
 # Importing `Model` also adds `lpd` and `main`
