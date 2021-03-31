46add5b85bc00da8281b30a65d34c04bdbb3feb7
zmoon <zmoon92@gmail.com>
2021-03-30 21:53:47 -0400

    More cleanup

--- blpd/bees.py
+++ blpd/bees.py
@@ -9,36 +9,38 @@ from scipy.stats import rv_continuous
 
 
 def get_step_length(*, l_0=1.0, mu=2.0):
-    """
+    r"""
     Draw a step length `l` from the distribution
-      p(l) = (l/l_0)^-mu
+
+    .. math::
+       p(l) = (l/l_0)^{-\mu}
 
     Parameters
     ----------
     l_0 : float
-        the minimum step size
-    mu : distribution shape parameter
-        =3 => Brownian motion
-        =2 => "super diffusive Lévy walk"
+        The minimum step size
+    mu : float
+        Distribution shape parameter
 
+        3 => Brownian motion
+
+        2 => "super diffusive Lévy walk"
     """
     if mu <= 1 or mu > 3:
         raise ValueError(f"`mu` should be in (1, 3] but is {mu!r}")
 
     q = np.random.rand()  # draw from [0, 1] (uniform)
     l = l_0 * (1 - q) ** (1 / (1 - mu))  # noqa: E741
-    # ^ note 1-mu not mu, this comes from the inverse of the CDF
+    # ^ note 1-mu not mu, which comes from the inverse of the CDF
 
     return l
 
 
-# scipy.stats.powerlaw doesn't let its param `a` be negative, so can't use it
+# Note: `scipy.stats.powerlaw` doesn't let its param `a` be negative, so can't use it
 
 
 class step_length_dist_gen(rv_continuous):
-    """Step length distribution class
-    using the scipy.stats machinery
-    """
+    """Step length distribution class using the `scipy.stats` machinery."""
 
     def _pdf(self, x, l_0, mu):
         c = -l_0 / (1 - mu)
@@ -48,61 +50,28 @@ class step_length_dist_gen(rv_continuous):
     def _cdf(self, x, l_0, mu):
         # https://www.wolframalpha.com/input/?i=anti-derivative+%28l%2Fc%29%5E-mu+dl
         # https://www.wolframalpha.com/input/?i=anti-derivative+%28l%2Fc%29%5E-mu+dl%2C+from+l%3Dc+to+x
-        # return l_0**mu * x**(1-mu) / (1-mu)
-        # return x * (x/l_0)**(-mu) / (1-mu)
-
         c = -l_0 / (1 - mu)
-        # if x >= l_0:
-        #     return x * (x/l_0)**(-mu) / (1-mu) + c
-        # else:
-        #     return 0
         # F = x * (x/l_0)**(-mu) / (1-mu) + c
         F = l_0 ** mu * x ** (1 - mu) / (1 - mu) + c
-        # F = l_0**mu * x**(1-mu) / (1-mu) + 0
         # F[x < l_0] = 0
         return F / c
 
-        # return (l_0 - x*(x/l_0)**(-mu)) / (mu-1)
-
-    # these forms seem to only work in a few special cases.
-    # if we don't provide _ppf it will numerically estimate it from _cdf
     def _ppf(self, q, l_0, mu):
-        # return (q*(mu-1) - l_0 + (1/l_0)**-mu)**(1/(1-mu))
-        # return (q*(mu-1) - l_0 + l_0**mu)**(1/(1-mu))
-
-        # x = (q*(mu-1) - l_0 + l_0**mu)**(1/(1-mu))
-        # # x[x < l_0] = np.nan
-        # return x
-
         c = -l_0 / (1 - mu)
-
-        # this won't work when `q*(1-mu) + l_0` is negative
         x = ((q * c * (1 - mu) + l_0) * l_0 ** -mu) ** (1.0 / (1.0 - mu))
 
         return x
 
-    #     # return ( (q-1)*(1-mu) / l_0**mu )**(1/(1-mu))
-
     def _argcheck(self, *args):
-        # below is the default
-        # --------------------
-        # cond = 1
-        # print(args)
-        # for arg in args:
-        #     cond = np.logical_and(cond, (np.asarray(arg) > 0))
-        # return cond
-
         l_0, mu = args  # unpack
 
-        # l_0 (minimum step length) must be positive
+        # Minimum step length `l_0` must be positive
         cond_l_0 = np.asarray(l_0) > 0
 
-        # shape parameter mu is supposed to be in range [1, 3]
+        # Shape parameter `mu` is supposed to be in range [1, 3]
         # not sure why it can't just be positive tho...
         arr_mu = np.asarray(mu)
         cond_mu = arr_mu > 1 and arr_mu <= 3
-
-        # return np.logical_and(cond_l_0, cond_mu, cond_mu_2)
 
         arr_l_0 = np.asarray(l_0)
         cond_mu_2 = 1 * (1 - arr_mu) + arr_l_0 >= 0
@@ -114,57 +83,41 @@ class step_length_dist_gen(rv_continuous):
         return cond
 
     def _get_support(self, *args, **kwargs):
-        # need to return support as 2-tuple
         return args[0], np.inf
 
 
-# step_length_dist = step_length_dist_gen(a=1.0, name="step_length_dist")
 step_length_dist = step_length_dist_gen(name="step_length_dist")
 
 
 # scaling with `scale` not working for pdf/cdf
 # need to specify them differently I guess
 class step_length_dist2_gen(rv_continuous):
-    """Step length distribution class
-    using the scipy.stats machinery
-    """
+    """Step length distribution class using the `scipy.stats` machinery."""
 
     # Instead of including l_0 as a param,
     # we just let scipy.stats do that via the `scale` param
 
     def _pdf(self, x, mu):
         c = -1.0 / (1 - mu)  # normalization const
-        # return x**-mu
         return (x ** -mu) / c
 
     def _cdf(self, x, mu):
         c = -1.0 / (1 - mu)
-        # F = x**(1-mu) / (1-mu) + c
         F = (x ** (1 - mu) - 1) / (1 - mu)
         return F / c
 
     def _ppf(self, q, mu):
-        # this won't work when `q*(1-mu) + 1` is negative
-        # x = (q*(1-mu) + 1) ** (1.0/(1.0-mu))
         c = -1.0 / (1 - mu)
         x = (q * c * (1 - mu) + 1) ** (1.0 / (1.0 - mu))
         return x
 
     def _argcheck(self, *args, **kwargs):
-
         mu = args  # unpack
 
-        # shape parameter mu is supposed to be in range [1, 3]
-        # not sure why it can't just be positive tho...
         arr_mu = np.asarray(mu)
         cond_mu = arr_mu > 1 and arr_mu <= 3
 
-        # return np.logical_and(cond_l_0, cond_mu)
         return cond_mu
-
-    # def _get_support(self, *args, **kwargs):
-    #     # need to return support as 2-tuple
-    #     return args[0], np.inf
 
 
 step_length_dist2 = step_length_dist2_gen(a=1.0, name="step_length_dist2")
@@ -176,11 +129,11 @@ if __name__ == "__main__":
     plt.close("all")
 
     # Set dist parameters
-    # Note that all 3 agree for mu=2.0, l_0=1.0...
-    # But the analytical CDFs do not agree with the hist for mu < 2
+    # Note that all 3 agree for mu=2.0, l_0=1.0 (I think the values used in the paper)
+    # But the analytical PDF/CDFs do not agree with the hist for mu < 2
     # Since the rvs results agree, this means the `_cdf` and `_pdf` methods are still off
     # though the `_ppf`s are fine.
-    mu = 1.2
+    mu = 2.2
     l_0 = 5
     assert 1 - mu + l_0 >= 0
     n_steps = int(5e5)
@@ -239,7 +192,6 @@ if __name__ == "__main__":
         cumulative=True,
         histtype="stepfilled",
         alpha=0.25,
-        # color="orange",
         label="orig drawing method",
     )
     ax2.hist(
